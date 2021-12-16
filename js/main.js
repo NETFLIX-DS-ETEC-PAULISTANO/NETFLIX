5a391bf5a47c6bd6e4ee8ab81587dc6a3ee6d50e
Vitor <vitornovaisv@gmail.com>
2021-12-16 20:46:29 -0300

    Troca dos Href para uma função que abre o modal

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const  BUTTON_ASSISTIR_DO_BANNER = document.getElementById('Assistir-banner');
-const  BUTTON_MAIS_INFORMAÇOES = document.getElementById('MaisInformacaos-banner');
+/*const  BUTTON_MAIS_INFORMAÇOES = document.getElementById('MaisInformacaos-banner');*/
 const ICON_DE_FECHAR_DO_MODAl_BANNER = document.getElementById('exit');
 const ITENS_DO_CARROSSEL = document.getElementsByClassName('item');
 
-BUTTON_MAIS_INFORMAÇOES.addEventListener( 'click', () => {
+/*BUTTON_MAIS_INFORMAÇOES.addEventListener( 'click', () => {*/
+
+    abrirmodal= () => {
 
     const BACKGROUND_MODAL = document.getElementsByClassName('banner-modal-container')[0];
     const MODAL = document.getElementsByClassName('banner-modal-flex-itens')[0];
@@ -33,7 +35,7 @@ BUTTON_MAIS_INFORMAÇOES.addEventListener( 'click', () => {
      
         `;
 
-})
+}
 ICON_DE_FECHAR_DO_MODAl_BANNER.addEventListener('click', () =>{
     const BACKGROUND_MODAL = document.getElementsByClassName('banner-modal-container')[0];
     BACKGROUND_MODAL.style.cssText = `
